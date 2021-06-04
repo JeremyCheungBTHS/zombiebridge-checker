0aec6f5c20910335ece0c735671819e44fa7e5d1
JeremyCheungBTHS <85359934+JeremyCheungBTHS@users.noreply.github.com>
2021-06-04 12:36:26 -0400

    Update ZombieBridgeCode.java

--- ZombieBridgeCode.java
+++ ZombieBridgeCode.java
@@ -1,3 +1,16 @@
 public class ZombieBridgeCode
 {
+ private int professor = 10;
+ private int you = 1;
+ private int assistant = 2; 
+ private int janitor = 5; 
+ public static void main(String[] args)
+ {
+   System.out.println("Hi. This is the Zombie Bridge Riddle Helper"); 
+   System.out.println("You (Y) - 1 min"); 
+   System.out.println("Assistant (A) - 2 min"); 
+   System.out.println("Janitor (J) - 5 min"); 
+   System.out.println("Professor (P) - 10 min"); 
+   System.out.println("Who are the first two to cross? (Two Characters)"); 
+ }
 }
